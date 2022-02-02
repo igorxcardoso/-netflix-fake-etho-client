6eace8a824ea9778cf03e7bc16a38fb0f1e63451
Igor Cardoso <iygorsilva@gmail.com>
2022-02-01 21:43:51 -0300

    validate with yup, and info useCallback

--- netflix-fake-client/src/screens/login/components/form/form.component.tsx
+++ netflix-fake-client/src/screens/login/components/form/form.component.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useState } from "react";
 import InputText from "../../../../components/inputs/input-text/input-text.component";
 import Button from "../../../../components/buttons/button/button.component";
+import * as yup from 'yup';
 
 export default function Form() {
   const [data, setData] = useState( {email: '', password: '' } )
@@ -12,19 +13,49 @@ export default function Form() {
         return {...prevState, [event.target.name]: event.target.value};
       });
     },
-
     /*deps*/
     [setData]
   )
 
-  console.log(data)
+  const validation = useCallback(
+    async () => {
+      // Definição do schema
+      const schema = yup.object().shape({
+        email: yup.string().required().email(),
+        password: yup.string().required()
+      });
+
+      try {
+        await schema.validate(data);
+        // console.log(true, data);
+        return true;
+
+      }catch(error: any) {
+        console.log(error.errors[0]);
+        return false;
+      }
+    },
+    [data]
+  );
+
+  const onSubmit = useCallback(
+    async () => {
+      await validation();
+
+      // console.log(data);
+    },
+    [validation]  // Vai atulizar o data que voi gravado na função onSumit
+  );             // Está falando para o Hook useCallback que deve ser redeclarado a cada mudança do data
+
+
+  // console.log(data)
 
 
   return (
     <>
       <InputText type={'text'} placeholder={'Email'} name={'email'} onChange={getChange} />
       <InputText type={'password'} placeholder={'Senha'} name={'password'} onChange={getChange} />
-      <Button primary={true}>{`Entrar`}</Button>
+      <Button onClick={onSubmit} primary={true}>{`Entrar`}</Button>
     </>
   );
 }
